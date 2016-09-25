8b5cd5288c8ad7767b8a50b998b201d6443f9e6f
Cesar <cesarbrazon10@gmail.com>
2016-09-25 14:54:45 -0400

    Changed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 def hello
-	rendef html: "A dani le gusta la marihuana y el sexo"
+	render html: 'A dani le gusta la marihuana y el sexo'
 end
 end
