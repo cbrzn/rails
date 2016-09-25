7e63591dc56a396f9bc74b7fcf24713fd6ecab15
Cesar <cesarbrazon10@gmail.com>
2016-09-25 14:37:57 -0400

    Add hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 def hello
-	rendef html: "hello, world!"
+	rendef html: "A dani le gusta la marihuana y el sexo"
 end
 end
